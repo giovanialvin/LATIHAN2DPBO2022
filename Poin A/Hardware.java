99e020d988758a89a87c96de8389d1cf9a3d345a
giovanialvin <99602640+giovanialvin@users.noreply.github.com>
2022-02-18 11:07:56 +0700

    Penambahan Komentar

--- Poin A/Hardware.java
+++ Poin A/Hardware.java
@@ -1,17 +1,22 @@
 import javax.print.event.PrintEvent;
 
 public class Hardware extends Product{
+    
+    //Deklarasi Atribut
     private String brand;
     private String model;
     
+    //Constructor kosong
     public Hardware() {
     }
-
+    
+    //Constructor
     public Hardware(String brand, String model) {
         this.setBrand(brand);
         this.setModel(model);
     }
-
+    
+    //Get Set Methods
     public void setModel(String model) {
         this.model = model;
     }
@@ -27,7 +32,8 @@ public class Hardware extends Product{
     public void setBrand(String brand) {
         this.brand = brand;
     }
-
+    
+    //Output Methods
     public void output_hardware(){
         System.out.println("Id Product          : "+ this.getId_Product());
         System.out.println("Price               : "+ this.getPrice());
