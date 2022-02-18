01f2f17f8eaa79504b744614cca7a8ffeccfcc4a
giovanialvin <99602640+giovanialvin@users.noreply.github.com>
2022-02-18 11:10:57 +0700

    Penambahan Komentar

--- Poin A/Main.java
+++ Poin A/Main.java
@@ -1,20 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("====================================");
+        //Instansiasi Objek
         Product pr = new Product();
         pr.setId_Product(1);
         pr.setPrice(15000);
+        //Output
         pr.output_product();
 
         System.out.println("====================================");
+        //Instansiasi Objek
         Hardware hd = new Hardware();
         hd.setId_Product(2);
         hd.setPrice(20000);
         hd.setBrand("Brand 1");
         hd.setModel("Model 1");
+        //Output
         hd.output_hardware();
-
+        
+        
         System.out.println("====================================");
+        //Instansiasi Objek
         Memory mr =  new Memory();
         mr.setId_Product(3);
         mr.setPrice(500000);
@@ -23,6 +29,7 @@ public class Main {
         mr.setFrequency("Frequency 1");
         mr.setMemorySize("Memory Size 1");
         mr.setSupportsCuda("Support Cuda 1");
+        //Output
         mr.output_memory();
         System.out.println("====================================");
 
