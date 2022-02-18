3fba621c3725c22a6cf15dcf96d3efbd5c0ebbad
giovanialvin <99602640+giovanialvin@users.noreply.github.com>
2022-02-18 11:12:53 +0700

    Penambahan Komentar

--- Poin B.cpp/Hardware.cpp
+++ Poin B.cpp/Hardware.cpp
@@ -1,20 +1,26 @@
 #include<string>
 #include<iostream>
 #include<string>
+//Pemanggilan File Parent
 #include"Product.cpp"
 
 using namespace std;
 
 class Hardware : public Product{
+    //Deklarasi Atribut
     private :
         string brand;
         string model;
 
     public :
+        
+        //Constructor
         Hardware(){
 
         }
-
+        
+    
+        //Get Set Methods
         void setBrand(string brand){
             this->brand = brand;
         }
@@ -30,14 +36,17 @@ class Hardware : public Product{
         string getModel(){
             return this->model;
         }
-
+        
+        //Output Method
         void output_Hardware(){
             cout << "Id Product                 : " << this->getId_Product() << endl;
             cout << "Price                      : " << this->getPrice() << endl;
             cout << "Brand                      : " << this->getBrand() << endl;
             cout << "Model                      : " << this->getModel() << endl;
         }
-
+        
+    
+        //Destructor
         ~Hardware(){
 
         }
