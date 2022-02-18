dbf141cd4589ae15f613d0101b8c4eb214d9499c
giovanialvin <99602640+giovanialvin@users.noreply.github.com>
2022-02-18 11:05:55 +0700

    Penambahan Komentar

--- Poin A/Product.java
+++ Poin A/Product.java
@@ -1,18 +1,23 @@
 public class Product{
     
-    
+    //Deklarasi Atribut
     private int price;
     private int id_Product;
-
+    
+    
+    //Constructor
     public Product(int price, int id_Product) {
         this.price = price;
         this.id_Product = id_Product;
     }
-
+    
+    
+    //Constructor Kosong
     public Product() {
     }
 
     
+    //Get Set Methods
     public int getId_Product() {
         return id_Product;
     }
@@ -25,7 +30,8 @@ public class Product{
     public void setId_Product(int id_Product) {
         this.id_Product = id_Product;
     }
-
+    
+    //Output Method
     public void output_product(){
         System.out.println("Id Product          : "+ this.getId_Product());
         System.out.println("Price               : "+ this.getPrice());
